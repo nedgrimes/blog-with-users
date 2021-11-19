26999f18975c923c3c2b441647de21af9c6c2f0e
frankgrimes <claudiopaci@me.com>
2021-11-19 16:39:38 +0100

    from sqlite to postgres

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, Table
 from functools import wraps
-from sqlalchemy.ext.declarative import declarative_base
+import os
 
 def admin_only(f): # f is edit_post func
     @wraps(f)
@@ -21,7 +21,8 @@ def admin_only(f): # f is edit_post func
     return decorated_func
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 ckeditor = CKEditor(app)
 # ckeditor.init_app(app)
@@ -30,7 +31,8 @@ Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
